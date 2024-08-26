3456d82dc0c8062baa262010457e52efec17f982
Fmarrero <facu_marrero@hotmail.com>
2024-08-26 20:06:57 -0300

    first commit

---
+++ src/main/java/hayTurnos/hayTurnos/HayTurnosApplication.java
@@ -0,0 +1,13 @@
+package hayTurnos.hayTurnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HayTurnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HayTurnosApplication.class, args);
+	}
+
+}
